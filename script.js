5a8f0c02fc526fe80c318e45f2b927cc92adbb02
Livara <livara@gmail.com>
2023-04-26 12:19:13 +0200

    Create contact page, add animation and link to my github and linkedIn account

--- script.js
+++ script.js
@@ -1,9 +1,59 @@
-// Get the h1 element
+// Name animation
 const title = document.getElementById("title");
 
-// Define a function to toggle the class
 function toggleFlash() {
-  title.classList.toggle("flash");
+title.classList.toggle("flash");
 }
-// Set an interval to toggle the class every 500ms
 setInterval(toggleFlash, 2000);
+
+
+
+//Contact Info 
+var textWrapper = document.querySelector('.ml1 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+.add({
+    targets: '.ml1 .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i+1)
+}).add({
+    targets: '.ml1 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: (el, i, l) => 80 * (l - i)
+}).add({
+    targets: '.ml1',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+});
+
+// About me animation
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+.add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+}).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+});
